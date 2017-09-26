5779c33ae01a6a01bca3507d748c9bfbcecca551
Jeff <coachmullen@gmail.com>
2017-09-25 18:22:59 -0700

    I simplified the code for clarity. The next step will be getting the hourly sales totals

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
 console.log();
@@ -13,9 +13,28 @@ var pike = {
   calculateHourlyOrders: function() {
     this.hourlyOrders = Math.ceil(Math.random() * (this.maxOrders - this.minOrders + 1) + this.minOrders);
     return Math.ceil(Math.random() * (this.maxOrders - this.minOrders + 1) + this.minOrders);
+  }
+};
+
+pike.calculateHourlyOrders();
+*/
+'use strict';
+
+var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
+console.log();
+
+var pike = {
+  minOrders: 23,
+  maxOrders: 65,
+  avgOrder: 6.3,
+  hourlyOrders: 0,
+  hourlyCookies: 0,
+  totalCookies: 0,
+  calculateHourlyOrders: function() {
+    return Math.ceil(Math.random() * (this.maxOrders - this.minOrders + 1) + this.minOrders);
   },
   calculateHourlyCookies: function() {
-    this.hourlyCookies = Math.ceil(this.calculateHourlyOrders() * this.avgOrder);
+    return Math.ceil(this.calculateHourlyOrders() * this.avgOrder);
   }
 };
 
