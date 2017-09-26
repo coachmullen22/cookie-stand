b97e28c43cfff477479a631fea16ed619855d295
Jeff <coachmullen@gmail.com>
2017-09-25 23:17:03 -0700

    working toward a solution

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+'use strict';
+
+var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
+var arrayLength = hours.length;
+var i;
+console.log();
+
+var pike = {
+  minOrders: 23,
+  maxOrders: 65,
+  avgOrder: 6.3,
+  hourlyOrders: 0,
+  hourlyCookies: 0,
+  totalCookies: 0,
+  calculateHourlyOrders: function() {
+    return Math.ceil(Math.random() * (this.maxOrders - this.minOrders + 1) + this.minOrders);
+  },
+  calculateHourlyCookies: function() {
+    return Math.ceil(this.calculateHourlyOrders() * this.avgOrder);
+  }
+};
+
+for (i = 0; i < arrayLength; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = (hours[i] + ' - ' + pike.calculateHourlyCookies());
+//    document.appendChild(liEl);
+  };
+
+pike.calculateHourlyOrders();
+pike.calculateHourlyCookies();
+
+
+
+
+
+
+
 /*'use strict';
 
 var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
@@ -18,25 +55,3 @@ var pike = {
 
 pike.calculateHourlyOrders();
 */
-'use strict';
-
-var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
-console.log();
-
-var pike = {
-  minOrders: 23,
-  maxOrders: 65,
-  avgOrder: 6.3,
-  hourlyOrders: 0,
-  hourlyCookies: 0,
-  totalCookies: 0,
-  calculateHourlyOrders: function() {
-    return Math.ceil(Math.random() * (this.maxOrders - this.minOrders + 1) + this.minOrders);
-  },
-  calculateHourlyCookies: function() {
-    return Math.ceil(this.calculateHourlyOrders() * this.avgOrder);
-  }
-};
-
-pike.calculateHourlyOrders();
-pike.calculateHourlyCookies();
