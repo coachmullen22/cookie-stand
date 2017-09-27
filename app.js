69ec99e8b6fff507b5c88e513d16d6b8c29dcbfc
Jeff <coachmullen@gmail.com>
2017-09-27 12:38:47 -0700

    just making sure my master is current

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var hours = ['6am-7am', '7am-8am', '8am-9am', '9am-10am', '10am-11am', '11am-Noo
 var allStores = [];
 
   function Store(location, minCust, maxCust, avgOrder) {
-    //An array to hold hourly customers data
     this.location = location;
     this.minCust = minCust;
     this.maxCust = maxCust;
@@ -13,23 +12,17 @@ var allStores = [];
     this.cookiesSoldHourly = [];
     this.dailyInventory = 0;
     for (var i = 0; i < hours.length; i++) {
-    this.cookiesSoldHourly.push(Math.ceil((Math.random() * (this.maxCust - this.minCust + 1) + this.minCust) * this.avgOrder));
+      this.cookiesSoldHourly.push(Math.ceil((Math.random() * (this.maxCust - this.minCust + 1) + this.minCust) * this.avgOrder));
   }
-    // Store.all.push(this);
-  }
-
-//    allStores.push(this);
-
-Store.prototype.cookiesSoldHourly = function() {
-
 }
-  console.log('hello world');
+//Store.prototype.cookiesSoldHourly = function() {
+//}
+
   var pike = new Store('1st and Pike', 23, 65, 6.3);
   var seaTac = new Store('SeaTac', 3, 24, 1.2);
   var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
   var capHill = new Store('Capitol Hill', 20, 38, 2.3);
   var alki = new Store('Alki', 2, 16, 4.6);
-// pike.cookiesSoldHourly();
 console.log(pike, seaTac, seaCenter, capHill, alki);
 
 /*
@@ -44,52 +37,4 @@ this.hourlySalesArray = function() {
 thEl.textContent = (hours[i]);
 trEl.appendChild(thEl);
 }
-*/
-/* Pike Place Store
-
-
-var pike = {
-  location: '1st and Pike',
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgOrder: 6.3,
-  hourlyCookies: 0,
-  totalCookies: 0,
-  calculateHourlyOrders: function() {
-    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
-  },
-  calculateHourlyCookies: function() {
-    return Math.ceil(this.calculateHourlyOrders() * this.avgOrder);
-  }
-};
-
-for (i = 0; i < array.length; i++) {
-  var liEl = document.createElement('li');
-  this.hourlyCookies = (this.calculateHourlyCookies());
-  this.totalCookies += this.hourlyCookies;
-  liEl.textContent = (hours[i] + ': ' + this.hourlyCookies);
-  document.body.appendChild(liEl);
-};
-
-var liEl = document.createElement('li');
-liEl.textContent = ('Daily Total : ' + this.totalCookies);
-document.body.appendChild(liEl);
-
-SeaTac Airport Store
-
-var hours = ['6am - 7am', '7am - 8am', '8am - 9am', '9am - 10am', '10am - 11am', '11am - Noon', 'Noon - 1pm', '1pm - 2pm', '2pm - 3pm', '3pm - 4pm', '4pm - 5pm', '5pm - 6pm', '6pm - 7pm', '7pm - 8pm'];
-console.log();
-
-var seaTac = {
-  minCustomers: 3,
-  maxCustomers: 24,
-  avgOrder: 1.2,
-  hourlyCookies: 0,
-  totalCookies: 0,
-
-  }
-};
-
-seaTac.calculateHourlyOrders();
-
 */
