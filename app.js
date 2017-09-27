bbe964f309202c3fcd31d8edf93e70b973147d62
Jeff <coachmullen@gmail.com>
2017-09-27 08:34:56 -0700

    major breakthrough. The array is showing for each store

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 'use strict';
-
 var cookieSalesTable = document.getElementById('')
-
 var hours = ['6am-7am', '7am-8am', '8am-9am', '9am-10am', '10am-11am', '11am-Noon', 'Noon-1pm', '1pm-2pm', '2pm-3pm', '3pm-4pm', '4pm-5pm', '5pm-6pm', '6pm-7pm', '7pm-8pm'];
-console.log();
-
 var allStores = [];
 
-  function Store(location, minCust, maxCust, avgOrder/*, hourlySales, dailyInventory*/) {
+  function Store(location, minCust, maxCust, avgOrder) {
+    //An array to hold hourly customers data
     this.location = location;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgOrder = avgOrder;
-    //An array to hold hourly sales data
-    this.hourlySalesArray = [];
+    this.hourlyCustomers = 0;
+    this.cookiesSoldHourly = [];
     this.dailyInventory = 0;
-    this.hourlySales = function() {
-      for (var i = 0; i < hours.length; i++) {
-      var randomHourlyCookies = Math.ceil((Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers) * this.avgOrder);
-      this.hourlySalesArray.push(randomHourlyCookies);
-      return randomHourlyCookies;
-    }
+    for (var i = 0; i < hours.length; i++) {
+    this.cookiesSoldHourly.push(Math.ceil((Math.random() * (this.maxCust - this.minCust + 1) + this.minCust) * this.avgOrder));
   }
-    allStores.push(this);
+    // Store.all.push(this);
   }
 
-new Store('1st and Pike', 23, 65, 6.3);
+//    allStores.push(this);
+
+Store.prototype.cookiesSoldHourly = function() {
+
+}
+  console.log('hello world');
+  var pike = new Store('1st and Pike', 23, 65, 6.3);
+  var seaTac = new Store('SeaTac', 3, 24, 1.2);
+  var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
+  var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+  var alki = new Store('Alki', 2, 16, 4.6);
+// pike.cookiesSoldHourly();
+console.log(pike, seaTac, seaCenter, capHill, alki);
 
 /*
 function makeTableRow() {
@@ -40,10 +45,6 @@ thEl.textContent = (hours[i]);
 trEl.appendChild(thEl);
 }
 */
-new Store('SeaTac', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Alki', 2, 16, 4.6);
 /* Pike Place Store
 
 
