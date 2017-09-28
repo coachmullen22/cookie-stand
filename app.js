2df36d934f91d01591994a79f492ec81b74058b6
Jeff <coachmullen@gmail.com>
2017-09-27 17:13:10 -0700

    Changed to prototype-based code

--- app.js
+++ app.js
@@ -8,16 +8,30 @@ var hours = ['6am-7am', '7am-8am', '8am-9am', '9am-10am', '10am-11am', '11am-Noo
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgOrder = avgOrder;
-    this.hourlyCustomers = 0;
-    this.cookiesSoldHourly = [];
+    this.cookiesSoldHourlyArray = [];
     this.dailyInventory = 0;
+    this.cookieSalesPerHour();
     allStores.push(this);
 }
 
-/*for (var i = 0; i < hours.length; i++) {
-  this.cookiesSoldHourly.push(Math.ceil((Math.random() * (this.maxCust - this.minCust + 1) + this.minCust) * this.avgOrder));
+Store.prototype.customersPerHour = function() {
+  return (Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust);
 }
-*/
+
+Store.prototype.cookieSalesPerHour = function() {
+  for (var i = 0; i < hours.length; i++) {
+  // var customersPerHour = this.customersPerHour();
+    this.cookiesSoldHourlyArray.push(Math.ceil(this.customersPerHour() * this.avgOrder));
+  }
+}
+
+
+  // for(var i = 0; i < hours.length; i ++){
+// }
+// return this.storeSalesTable;
+// Store.prototype.render = function() {
+// }
+
   var pike = new Store('1st and Pike', 23, 65, 6.3);
   var seaTac = new Store('SeaTac', 3, 24, 1.2);
   var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
@@ -26,18 +40,6 @@ var hours = ['6am-7am', '7am-8am', '8am-9am', '9am-10am', '10am-11am', '11am-Noo
 console.log(pike, seaTac, seaCenter, capHill, alki);
 
 
-/*Store.prototype.render = function() {
-  var trEL = document.createElement('tr');
-
-  var tdEl = document.createElement('td');
-  tdEl.textContent = this.cookiesSoldHourly;
-  trEl.appendChild(tdEl);
-}
-
-/*this.hourlySalesArray = function() {
-  for (var i = 0; i < hours.length; i++)
-  thEl.textContent = (hours[i]);
-  trEl.appendChild(thEl); */
 
 function makeHeaderRow() {
 // create tr
@@ -62,11 +64,27 @@ function makeHeaderRow() {
   storeSalesTable.appendChild(trEl);
 }
 
-/*function storeRowsFTW() {
-  for(var i in ) {
-    [i].render();
-  }
+function makeDataRow() {
+
 }
-*/
+/*Store.prototype.render = function() {
+var trEL = document.createElement('tr');
+
+var tdEl = document.createElement('td');
+tdEl.textContent = this.cookiesSoldHourly;
+trEl.appendChild(tdEl);
+}
+
+/*this.hourlySalesArray = function() {
+for (var i = 0; i < hours.length; i++)
+thEl.textContent = (hours[i]);
+trEl.appendChild(thEl); */
+
+// function storeRowsFTW(storeRows) {
+//   for(var i in ) {
+//     [i].render();
+//   }
+// }
+
 makeHeaderRow();
 //storeRowsFTW();
